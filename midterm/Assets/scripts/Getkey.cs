2a5a52825302dfdf07542b07fa2bcb8d69387f2b
ericaliuser <ericaliu@ericas-mbp.home>
2014-07-29 09:13:41 -0400

    3rd repo

--- midterm/Assets/scripts/Getkey.cs
+++ midterm/Assets/scripts/Getkey.cs
@@ -4,10 +4,12 @@ using System.Collections;
 public class Getkey : MonoBehaviour {
 	public Transform key; 
 	public Transform text1; 
+	public Transform invisible;
 
 	void OnTriggerEnter ( Collider player ){
 		Destroy (key.gameObject ); 
-		Destroy (text1.gameObject );
+		Destroy (text1.gameObject ); 
+		Destroy (invisible.gameObject ); 
 		Destroy (gameObject );
 	}
 }
